da2e4cf411cddfa2693affd152c4a3fdd8079582
MaBED18 <128619327+MaBED18@users.noreply.github.com>
2023-06-01 09:56:57 -0700

    serilization

--- hello.py
+++ hello.py
@@ -1,5 +1,24 @@
 #### Mark's section ####
+import pickle
+from os import path
 
+def save (object, file):
+    """This serilizes the dictornary"""
+    with open(file, "wb") as f:
+        pickle.dump(object, f)
+
+def load(file):
+    """this loads the dictonary and returns a file, if there isn't one then it returns None"""
+    if path.exists(file):
+        with open(file, "rb") as f:
+            return pickle.load(f)
+    return None
+
+
+USERS_FILE = "users.pkl"
+users = load(USERS_FILE)
+if users is None:
+  users = {}
 
 
 
