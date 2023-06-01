c00f660cc8bd9c4fba0b2c27b492be85eda643dd
BellaMorisseau <128619317+BellaMorisseau@users.noreply.github.com>
2023-06-01 10:18:20 -0700

    I added new code that gets the user's input for signing up and log in usernames into Rachelle's section

--- hello.py
+++ hello.py
@@ -27,9 +27,26 @@ if users is None:
 while True:
     print ("")
     print ("")
-    user = input("Enter a username, or q to quit, u to see all users: ").capitalize()
+    question_1 = input("Would you like to log into your account or sign up and create a new profile? Press L for log in or S to sign up:")
+    
+    
+    if question_1 == "S": 
+        user = input("Please enter a username that you would like to use.").capitalize()
+
+    elif question_1 == "L":
+        print("L")
+
+
+
+
+<<<<<<< HEAD
+    #####Bella's Pretty Section#####
+=======
 
 
 
 
     #####Bella's Pretty Section#####
+
+
+>>>>>>> 629d8c1 (I added new code that gets the user's input for signing up and log in usernames into Rachelle's section)
