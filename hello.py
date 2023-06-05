f2af2661d1ed23b1e0fb658126e7b19a50254604
MaBED18 <128619327+MaBED18@users.noreply.github.com>
2023-06-04 23:09:27 -0700

    Adding final coding logic

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-#### Mark's section ####
+
 import pickle
 from os import path
 
@@ -22,8 +22,8 @@ if users is None:
 
 
 
+total = 2
 
-### Rachelle's section ###
 while True:
     print ("")
     print ("")
@@ -31,23 +31,38 @@ while True:
     
     
     if question_1 == "S": 
-        user = input("Please enter a username that you would like to use.").capitalize()
-        pw_hash = hash(input("please enter your password: "))
+        user = input("Please enter a username that you would like to use: ")
+        pw_hash = hash(input("Please enter your password: "))
         users[user] = pw_hash
     elif question_1 == "L":
-        question_2 = input ("Please enter your username: ").capitalize
+        question_2 = input ("Please enter your username: ")
         if question_2 in users:
-            question_3 = ("Please enter your password: ")
-            if question_3 == pw_hash[question_2]:
+            question_3 = input ("Please enter your password: ")
+            if hash(question_3) == pw_hash:
+                print ("")
                 print ("Welcome back user " , question_2)
-            elif question_3 != pw_hash[question_2]:
-                print ("that is the wrong password please sign up: ")
-                user = input("Please enter a username that you would like to use.").capitalize()
+            elif hash(question_3) != pw_hash:
+                while total > 0:
+                    print ("")
+                    print("That is the incorrect password, please try again.")
+                    print("")
+                    question_3 = input ("Please enter your password: ")
+                    if hash(question_3) == pw_hash:
+                        print ("")
+                        print ("Welcome back user " , question_2)
+                        total = 2
+                        break
+                    elif hash(question_3) != pw_hash:
+                        total -= 1
+                        continue
+                if total == 0:
+                    print ("You have entered the wrong password too many times please sign up instead. ")
+                    user = input("Please enter a username that you would like to use: ")
+                    pw_hash = hash(input("please enter your password: "))
+                    users[user] = pw_hash
+                    total = 2
+        elif question_2 != user:
+                print ("there is no username that matches that, please sign up.")
+                user = input("Please enter a username that you would like to use: ").capitalize()
                 pw_hash = hash(input("please enter your password: "))
                 users[user] = pw_hash
-
-        elif question_2 not in users:
-            print ("that is the wrong password please sign up: ")
-            user = input("Please enter a username that you would like to use.").capitalize()
-            pw_hash = hash(input("please enter your password: "))
-            users[user] = pw_hash
